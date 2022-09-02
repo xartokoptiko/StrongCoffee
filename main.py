635629bbba6fac5169ad2fa3e4d3ebe53f28bfca
lampros <lamproskarachristos@yahoo.com>
2022-09-02 14:49:05 +0300

    first commit ! <3

---
+++ main.py
@@ -0,0 +1,23 @@
+"""
+
+    @Author : CoffeeDrinkersTeam
+
+    @Description : 
+        - Having an old yet lovely Computer ?
+        - Trying to develop a simple or complex(oh boy good luck...) program with java ?
+        - Your computer can't handle InteliJ IDEA, VS Code , eclipse etc....
+
+        Don't be stupid and use linux distriputions where you can do a banch more usefull 
+        stuff and literally free your computer from the chains of microsoft. Those are for
+        people who respect themselves ,wich we and you are cleary dont. Simply use our great
+        with no bugs at all open source code to do the job. <3 !
+
+        -Now that we laughed(not) with my houmor. This is an open-source code 
+        that simply does the dirty work for you when writing code with java, something 
+        like what InteliJ idea does when you hit the run button, but those guys really
+        know what they're doing unlike us.
+
+        We did it mostly for practice and test our knowlegde. Hope you like it.
+        More than happy to accept your thoughts!.
+
+"""

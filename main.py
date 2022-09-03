e4d1d7cf57256f2e3a5bb83f162e4ecec337d259
Lampros <lamproskarachristos@yahoo.com>
2022-09-03 22:31:01 +0300

    Changes on main file

--- main.py
+++ main.py
@@ -27,11 +27,56 @@ from Data import data
 
 class Main:
     def __init__(self):
-        data_base = data.Data()
-        # data_base.save_new_project("MyProject", "/projects/python/FUUUUCK")
-        # data_base.update_now('MyProject')
-        # print(data_base.get_projects())
+        self.data_base = data.Data()
+        self.data_base.save_new_project("project_one", "/path/one")
+        self.data_base.save_new_project("project_two", "/path/two")
+        self.data_base.save_new_project("project_three", "/path/three")
 
+        print("~~~ [Strong Coffee Builder] ~~~")
+        while True:
+            print("""
+-------------------
+[1] See & Select Projects
+[2] Create Project
+[3] About
+[4] Help
+[5] Safe Exit
+-------------------
+            """)
+            command = int(input(">>>"))
+            if command == 1:
+                self.see_projects()
+            else:
+                print("[*] Invalid command ->", command)
+            
+    def see_projects(self):
+        projects = self.data_base.get_projects()
+        print("-----[Projects]------")
+        for project in projects:
+            print("-[{}] {}".format( projects.index(project) ,project[0]))
+        print("-[b] Back")
+        command = input(">>>")
+        if command == "b":
+            Main()
+        else :
+            self.selected_project(projects[int(command)])
+
+    def selected_project(self, project):
+        print("""
+--------[{}]--------
+--[1] build Project
+--[2] Build & Run Project
+--[b] Back
+        """.format(project[0]))
+        command = input(">>>")
+        if command == "b":
+            self.see_projects
+
+        elif int(command) == 1:
+            print("BUILDING!!!!!", project[0])
+
+        elif int(command) == 2:
+            print("BUILDING AND RUNNING!!!!!!", project[0])
 
 if __name__ == "__main__":
     Main()

4564d05e66fd85d8d0fceb24a9d25ec94c46c29b
lampros <lamproskarachristos@yahoo.com>
2022-09-03 20:16:47 +0300

    added Functions in data file for editing data

--- Data/data.py
+++ Data/data.py
@@ -15,3 +15,22 @@ class Data:
         self.cursor.execute("""CREATE TABLE IF NOT EXISTS Projects (project_name TEXT
         , project_dir TEXT, created DATE, updated DATE) 
     """)
+        self.connection.commit()
+
+    def save_new_project(self, project_name, project_dir):
+        self.cursor.execute("""INSERT INTO Projects(project_name, project_dir, created, updated)
+         VALUES (?,?, strftime('%Y-%m-%d %H-%M-%S','now'), strftime('%Y-%m-%d %H-%M-%S','now') )""", (project_name, project_dir))
+        self.connection.commit()
+
+    def drop_table(self):
+        self.cursor.execute("DROP TABLE Projects")
+        self.connection.commit()
+
+    def update_now(self, project_name):
+        self.cursor.execute("""UPDATE Projects SET updated=strftime('%Y-%m-%d %H-%M-%S','now') WHERE project_name=? """,
+                            [project_name])
+        self.connection.commit()
+
+    def get_projects(self):
+        self.cursor.execute("SELECT * FROM Projects")
+        return self.cursor.fetchall()
--- main.py
+++ main.py
@@ -7,17 +7,17 @@
         - Trying to develop a simple or complex(oh boy good luck...) program with java ?
         - Your computer can't handle InteliJ IDEA, VS Code , eclipse etc....
 
-        Don't be stupid and use linux distriputions where you can do a banch more usefull 
+        Don't be stupid and use linux distributions where you can do a bunch more useful
         stuff and literally free your computer from the chains of microsoft. Those are for
-        people who respect themselves ,wich we and you are cleary dont. Simply use our great
+        people who respect themselves ,which we and you are clearly not. Simply use our great
         with no bugs at all open source code to do the job. <3 !
 
-        -Now that we laughed(not) with my houmor. This is an open-source code 
+        -Now that we laughed(not) with my humor. This is an open-source code
         that simply does the dirty work for you when writing code with java, something 
         like what InteliJ idea does when you hit the run button, but those guys really
         know what they're doing unlike us.
 
-        We did it mostly for practice and test our knowlegde. Hope you like it.
+        We did it mostly for practice and test our knowledge. Hope you like it.
         More than happy to accept your thoughts!.
 
 """
@@ -27,7 +27,10 @@ from Data import data
 
 class Main:
     def __init__(self):
-        data.Data()
+        data_base = data.Data()
+        # data_base.save_new_project("MyProject", "/projects/python/FUUUUCK")
+        # data_base.update_now('MyProject')
+        # print(data_base.get_projects())
 
 
 if __name__ == "__main__":
